ba8723b496c01dd83ba04b9a9fa904d70ed06a08
yakuphantonintshi <yakupha58756@gmail.com>
2024-06-13 16:39:56 +0200

    Update Admin

--- script/admin.js
+++ script/admin.js
@@ -1,5 +1,4 @@
 let products =  JSON.parse(localStorage.getItem('products'))
-let table = document.getElementsByTagName('table')
 let container = document.querySelector('[flowerP]')
 
 products.forEach(product => {
@@ -12,5 +11,37 @@ products.forEach(product => {
         <td>${product.Price}</td>
     </tr>`
 })
+
+let btnSaveItem = document.querySelector('#modal-btn');
+let btnClose = document.querySelector('#modal-close');
+let prodName = document.querySelector('#prodName');
+let prodCat = document.querySelector('#prodCat')
+let prodDescr = document.querySelector('#prodDescr')
+let prodImage = document.querySelector('#prodImg')
+let prodPrice = document.querySelector('#prodPrice')
+
+function Product(name, category, description, image, price){
+    this.name = name;
+    this.category = category;
+    this.description = description;
+    this.image = image;
+    this.price = price;
+}
+btnSaveItem.addEventListener('click', ()=>{
+    let name = prodName.value;
+    let category = prodCat.value;
+    let description= prodDescr.value;
+    let image = prodImage.value;
+    let price = prodPrice.value;
+
+    let NewProduct = new Product(name, category, description, image, price)
+    let products = JSON.parse(localStorage.getItem('products'))
+    products.push(NewProduct)
+
+    localStorage.setItem('products' , JSON.stringify(products))
+
+
+})
+
 
 
